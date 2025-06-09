eabfc672a500be31370f7d1bcf96c12647c38ff7
Sebastian Zapata Restrepo <sebastian.zapata@grupocnet.com>
2025-06-09 16:48:46 -0500

    bancow port

--- src/domain/ports/BancowPort.js
+++ src/domain/ports/BancowPort.js
@@ -1,10 +1,26 @@
 export class BancowPort {
 
     async verificarDuplicados(input) {
-        throw new Error("Method not implemented.");
+        throw new Error("Method verificarDuplicados not implemented.");
     }
 
-    async obtenerPorEstado(estado) {
-        throw new Error("Method not implemented.");
+    async insertarRegistro(input) {
+        throw new Error("Method insertarRegistro not implemented.");
+    }
+
+    async obtenerTodos() {
+        throw new Error("Method obtenerTodos not implemented.");
+    }
+
+    async obtenerPorIdFlujoRegistro(id) {
+        throw new Error("Method obtenerPorIdFlujoRegistro not implemented.");
+    }
+
+    async deletePorIdFlujoRegistro(id) {
+        throw new Error("Method deletePorIdFlujoRegistro not implemented.");
+    }
+
+    async createUserAccount() {
+        throw new Error("Method createUserAccount not implemented.");
     }
 }
