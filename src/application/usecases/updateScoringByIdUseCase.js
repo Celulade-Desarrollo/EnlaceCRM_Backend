fbe754d8d749d6f0daf1ace33cfaf2d99cfd6198
Sebastian Zapata Restrepo <sebastian.zapata@grupocnet.com>
2025-06-12 16:04:01 -0500

    Correxion de updateScoringUseCase funciones actualizarEstado

--- src/application/usecases/updateScoringByIdUseCase.js
+++ src/application/usecases/updateScoringByIdUseCase.js
@@ -1,11 +1,11 @@
 import { scoringRepository } from "../../infrastructure/repositories/scoring.repository.js";
 
 export async function updateScoringByIdUseCase(id, estado = "pendiente") {
-  const existe = await scoringRepository.existePorIdFlujo(id);
+  const existe = await scoringRepository.obtenerPorId(id);
   if (!existe) {
     throw new Error("Registro no encontrado");
   }
 
-  await scoringRepository.actualizarEstado(id, estado);
+  await scoringRepository.actualizarEstadoPorId(id, estado);
   return { mensaje: "Registro actualizado correctamente" };
 }
