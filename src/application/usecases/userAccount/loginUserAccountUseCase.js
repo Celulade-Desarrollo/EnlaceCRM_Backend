e4bc5b4628f412ad3fc65e8bbd69f2ee90472288
Sebastian Zapata <sebastian.zapata@grupocnet.com>
2025-07-08 11:03:53 -0500

    Estado de cuenta endpoint /api/user/estado-cuenta/:idUsuario

--- src/application/usecases/userAccount/loginUserAccountUseCase.js
+++ src/application/usecases/userAccount/loginUserAccountUseCase.js
@@ -2,15 +2,22 @@ import { userAccountService } from "../../services/userAccountServiceInstance.js
 import { tokenVerifierService } from "../../services/TokenVerifierService.js";
 import { tokenGeneratorService } from "../../services/TokenGeneratorService.js";
 import { fetchNbCliente } from "../../services/fetchNbCliente.js";
-import { fetchLoginAlpina } from "../../services/fetchAlpina.js";
+import { alpinaService } from '../../services/alpinaServiceInstance.js';
+import { AuthAlpinaAdapter } from '../../../infrastructure/adapters/AuthAlpinaAdapter.js';  
 
 export async function loginUserAccountUseCase(nbCliente, nbAgenteComercial, token){
-    const bearerToken = await fetchLoginAlpina()
+    const authAdapter = new AuthAlpinaAdapter();
+    const bearerToken = await authAdapter.obtenerToken();
+
     const cedula = await fetchNbCliente(nbCliente, nbAgenteComercial, bearerToken)
 
 
     const cuenta = await userAccountService.validarCuentaCedula(cedula)
-    if(!cuenta) throw new Error("Cuenta no existente")
+    if(!cuenta) throw new Error({
+        message: "Cuenta no encontrada",
+        nbCliente: nbCliente,
+        nbAgenteComercial: nbAgenteComercial
+    })
 
     const Token = token
     if(!Token) throw new Error("Falta parametro Token")
@@ -22,6 +29,7 @@ export async function loginUserAccountUseCase(nbCliente, nbAgenteComercial, toke
     if(!tokenTenderoEnlaceCRM) throw new Error("Falló la creación del token para el tendero")
 
     return{
+     
         token: tokenTenderoEnlaceCRM,
         tipo: "usuario",
         state: "Authenticated"
