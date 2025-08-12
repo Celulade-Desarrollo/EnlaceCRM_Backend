b2dfcbcc29098e43e47553a8fca702265b9ec719
Sebastian Zapata <sebastian.zapata@grupocnet.com>
2025-08-12 15:34:40 -0500

    token

--- src/application/usecases/userAccount/loginUserAccountUseCase.js
+++ src/application/usecases/userAccount/loginUserAccountUseCase.js
@@ -26,7 +26,7 @@ export async function loginUserAccountUseCase(nbCliente, nbAgenteComercial, toke
         throw error
     }
 
-    /*
+    
 
     const Token = token
     if(!Token) throw new Error("Falta parametro Token")
@@ -37,7 +37,7 @@ export async function loginUserAccountUseCase(nbCliente, nbAgenteComercial, toke
         error.status = 401;
         throw error;
     }
-*/
+
     const tokenTenderoEnlaceCRM = await tokenGeneratorService.generateToken({cedula: cedula})
     if(!tokenTenderoEnlaceCRM) throw new Error("Falló la creación del token para el tendero")
 
