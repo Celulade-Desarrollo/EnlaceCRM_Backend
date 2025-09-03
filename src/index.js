7b7e55d42e57ddec5c8833ffa92c20e21f4b25b4
Sebastian Zapata <sebastian.zapata@grupocnet.com>
2025-09-03 10:17:05 -0500

    hotfix corc config

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://enlace-crm.com",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
