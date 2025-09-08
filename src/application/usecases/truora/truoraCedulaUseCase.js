8db03b49ef0cf84e0dcfb5cc1667d5b793e3d973
Sebastian Zapata <sebastian.zapata@grupocnet.com>
2025-09-08 15:19:02 -0500

    truora test

--- src/application/usecases/truora/truoraCedulaUseCase.js
+++ src/application/usecases/truora/truoraCedulaUseCase.js
@@ -1,7 +1,7 @@
 import { TruoraService } from "../../services/TruoraService.js";
 
-export async function truoraCedulaUseCase(info) {
-    const persona = await TruoraService.verificarInfoCedula(info);
+export async function truoraCedulaUseCase(cedula) {
+    const persona = await TruoraService.verificarInfoCedula(cedula);
     if(!persona || persona.length === 0) throw new Error("No se encontró información para la cédula proporcionada.");
     
     console.log("Registro de truora",persona);
--- src/infrastructure/adapters/TruoraAdapter.js
+++ src/infrastructure/adapters/TruoraAdapter.js
@@ -2,7 +2,7 @@ import { truoraRepository } from "../repositories/truora.repository.js";
 import { TruoraPort } from "../../domain/ports/TruoraPort.js";
 
 export class TruoraAdapter extends TruoraPort {
-    async verificarInfoCedula(info) {
-        return await truoraRepository.verificarInfoCedula(info)
+    async verificarInfoCedula(cedula) {
+        return await truoraRepository.verificarInfoCedula(cedula)
     }
 }
