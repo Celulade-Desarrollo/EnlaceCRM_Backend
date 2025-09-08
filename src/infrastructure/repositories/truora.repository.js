1420ce1939e35b79c7d93d817b04550b46b01493
Sebastian Zapata <sebastian.zapata@grupocnet.com>
2025-09-08 15:27:09 -0500

    truora status

--- src/infrastructure/repositories/truora.repository.js
+++ src/infrastructure/repositories/truora.repository.js
@@ -1,11 +1,26 @@
 export const truoraRepository = {
-    async verificarInfoCedula(cedula) {
-        const { document_number } = cedula;
+    async verificarInfoCedula(data) {
+        const { document_number, validation_status } = data;
         const Cedula_Cliente = document_number;
         const pool = await poolPromise;
+
+      
         const result = await pool.request()
             .input("Cedula_Cliente", sql.NVarChar, Cedula_Cliente)
             .query("SELECT * FROM FlujosRegistroEnlace WHERE Cedula_Cliente = @Cedula_Cliente");
+
+        
+        if (result.recordset.length > 0) {
+            await pool.request()
+                .input("Cedula_Cliente", sql.NVarChar, Cedula_Cliente)
+                .input("TruoraStatus", sql.NVarChar, validation_status)
+                .query(`
+                    UPDATE FlujosRegistroEnlace 
+                    SET truora = @TruoraStatus 
+                    WHERE Cedula_Cliente = @Cedula_Cliente
+                `);
+        }
+
         return result.recordset;
     }
 }
