90ebdce9a4928c3c6d576afcea4a6b8f6637726e
Sebastian Zapata <sebastian.zapata@grupocnet.com>
2025-09-18 10:44:04 -0500

    truora data cc

--- src/infrastructure/repositories/truora.repository.js
+++ src/infrastructure/repositories/truora.repository.js
@@ -28,30 +28,39 @@ export const truoraRepository = {
     },
 
 
-    async setTruoraStatusProcess(cedula, status) {
-    try {
-        const pool = await poolPromise;
+    async setTruoraStatusProcess(cedula, status, birth_date, birth_department, birth_place, expedition_place, issue_date) {
+        try {
+            const pool = await poolPromise;
 
-      
-        const result = await pool.request()
-            .input("Cedula_Cliente", sql.NVarChar, cedula)
-            .input("TruoraStatus", sql.NVarChar, status)
-            .query(`
-                UPDATE FlujosRegistroEnlace 
-                SET truora = @TruoraStatus 
-                WHERE Cedula_Cliente = @Cedula_Cliente;
-            
-                SELECT * FROM FlujosRegistroEnlace 
-                WHERE Cedula_Cliente = @Cedula_Cliente;
-            `);
-        
-        if (result.recordset.length === 0) {
-            throw new Error("No se encontró registro con esa cédula");
+            const result = await pool.request()
+                .input("Cedula_Cliente", sql.NVarChar, cedula)
+                .input("TruoraStatus", sql.NVarChar, status)
+                .input("BirthPlace", sql.NVarChar, birth_place)
+                .input("BirthDepartment", sql.NVarChar, birth_department)
+                .input("BirthDate", sql.NVarChar, birth_date)
+                .input("ExpeditionPlace", sql.NVarChar, expedition_place)
+                .input("IssueDate", sql.NVarChar, issue_date)
+                .query(`
+                    UPDATE FlujosRegistroEnlace 
+                    SET truora = @TruoraStatus,
+                        birth_place = @BirthPlace,
+                        birth_department = @BirthDepartment,
+                        birth_date = @BirthDate,
+                        expedition_place = @ExpeditionPlace,
+                        issue_date = @IssueDate
+                    WHERE Cedula_Cliente = @Cedula_Cliente;
+
+                    SELECT * FROM FlujosRegistroEnlace 
+                    WHERE Cedula_Cliente = @Cedula_Cliente;
+                `);
+
+            if (result.recordset.length === 0) {
+                throw new Error("No se encontró registro con esa cédula");
+            }
+
+            return result.recordset[0];
+        } catch (error) {
+            throw new Error(`Error actualizando estado Truora: ${error.message}`);
         }
-
-        return result.recordset[0];
-    } catch (error) {
-        throw new Error(`Error actualizando estado Truora: ${error.message}`);
     }
-}
 }
