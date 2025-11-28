9c8a831f9ded8e13462fd4fd46bad00b01cd8536
Sebastian Zapata <sebastian.zapata@grupocnet.com>
2025-11-28 15:31:32 -0500

    Actualizar entradas de la funcion a Intereses y Fees asi como la suma de estos

--- src/application/usecases/pagos/registrarMovimientoTipoDosUseCase.js
+++ src/application/usecases/pagos/registrarMovimientoTipoDosUseCase.js
@@ -1,12 +1,14 @@
 import {estadoCuentaService} from "../../services/estadoCuentaServiceInstance.js"
 
-export async function registrarMovimientoTipoDosUseCase(IdMovimiento, monto){
+export async function registrarMovimientoTipoDosUseCase(IdMovimiento, monto, Intereses, Fees){
 
     const data = await estadoCuentaService.traerPorIdMovimiento(IdMovimiento);
 
    const nuevoMontoModificado = data.AbonoUsuario + monto 
+   const nuevoInteresesModificado = data.Intereses + Intereses
+   const nuevoFeesModificado = data.Fees + Fees
 
-    const recorset =  await estadoCuentaService.registrarMovimientoAbono(IdMovimiento, nuevoMontoModificado);
+    const recorset =  await estadoCuentaService.registrarMovimientoAbono(IdMovimiento, nuevoMontoModificado, nuevoInteresesModificado, nuevoFeesModificado);
     return {
         status: "success",
         data: recorset
