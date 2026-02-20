e91e42c92ad17ef1ed4b0b180d482bd734f870cb
jhon328 <jhon.gomez@grupocnet.com>
2026-02-20 08:59:32 -0500

    Feature/ajuste get dispersion banco (#139)
    * update docker compose port deploy develop statement
    
    * remove ports
    
    * Feature/campo nombre tienda (#118)
    
    * Docker compose develop  (#116)
    
    * Tesoreria
    
    * update tesoreria model and controllers for repo
    
    * docker-compose-develop.yml
    
    * se ajuste el repositorio de tesorereria (#117)
    
    Co-authored-by: Over Isai Medina Muñoz <mediov@compunet.local>
    
    * feat: agregar campo nombre de la tienda al flujo de registro
    
    ---------
    
    Co-authored-by: sebaszapata18674 <sebastian.zapata@grupocnet.com>
    Co-authored-by: Over Isai Medina Muñoz <mediov@compunet.local>
    
    * Feature/campo nombre tienda (#121)
    
     Agregado input parameter Nombre_Tienda en insertarRegistro
    - Actualizado query INSERT para incluir columna Nombre_Tienda
    - Campo tipo NVARCHAR para almacenar nombre del negocio del cliente
    
    * Change the implementation statement to pm2 instead of docker (#124)
    
    * Feature/surti estado cupo disponible (#129)
    
    * se ajusta reporitory y caso de uso de login para el estado negado
    
    * Docker compose develop  (#116)
    
    * Tesoreria
    
    * update tesoreria model and controllers for repo
    
    * docker-compose-develop.yml
    
    * se ajuste el repositorio de tesorereria (#117)
    
    Co-authored-by: Over Isai Medina Muñoz <mediov@compunet.local>
    
    * se crea nuevo endpoit para validar estado del cupo disponible y rango
    
    * se ajusta caso de uso y nuevas funciones repositorio para endpoint de estadosCupodisponible
    
    * ajuste de estados
    
    * se ajusta datos que se piden para usar el endpoint y datos que responde
    
    * se crea nuevo endpoint para tarer todos los estados de cupo
    
    * se ajusta consulta de repositorio para busacar todos los usuairo y su estado correspondiente
    
    * se crean 2 endpint para trar informacion y estdo actual del cupo de cliente
    
    ---------
    
    Co-authored-by: sebaszapata18674 <sebastian.zapata@grupocnet.com>
    Co-authored-by: Medina0604 <over.medina@grupocnet.com>
    Co-authored-by: Over Isai Medina Muñoz <mediov@compunet.local>
    
    * ajuste cd archivo yml (#131)
    
    * ajuste de conuslta get dispersion banco
    
    ---------
    
    Co-authored-by: Sebastian Zapata <sebastian.zapata@grupocnet.com>
    Co-authored-by: Medina0604 <over.medina@grupocnet.com>
    Co-authored-by: Over Isai Medina Muñoz <mediov@compunet.local>

---
+++ src/application/usecases/flujoRegistro/consultarPorCedulaYNbClienteUseCase.js
@@ -0,0 +1,96 @@
+import { flujoRegistroService } from "../../services/flujoRegistroServiceInstance.js";
+
+function calcularCupo(valor) {
+  if (valor >= 40000 && valor <= 50000) return 1;
+  if (valor > 50000 && valor <= 100000) return 2;
+  if (valor > 100000 && valor <= 150000) return 3;
+  if (valor > 150000 && valor <= 200000) return 4;
+  if (valor > 200000 && valor <= 300000) return 5;
+  if (valor > 300000 && valor <= 400000) return 6;
+  if (valor > 400000 && valor <= 500000) return 7;
+  if (valor > 500000 && valor <= 750000) return 8;
+  if (valor > 750000 && valor <= 1000000) return 9;
+  if (valor > 1000000) return 10;
+  return 0;
+};
+
+export async function consultarPorCedulaYNbClienteUseCase(
+  nbCliente
+) {
+  const registro = await flujoRegistroService.obtenerEstadoYCupo(
+      nbCliente
+    );
+    console.log(`registro-`,registro)
+
+  if (!registro) {
+    return {
+      nbCliente,
+      Estado: 0
+    };
+  }
+
+  let estado = 0;
+  let subEstado = null;
+  let cupo = null;
+
+  // BLOQUEO
+  if (registro.BloqueoPorMora === true) {
+    estado = 4;
+  }
+  
+  //APROBADO 
+  else if (registro.CupoDisponible > 0) {
+    estado = 3;
+    cupo = calcularCupo(registro.CupoDisponible);
+  }
+
+  // en APROBACIÓN
+ else {
+  estado = 2;
+
+  const scoring =
+    await flujoRegistroService.obtenerScoringPorFlujo(
+      registro.Id
+    );
+
+  //Subestado 1: Pendiente Enlace
+  if (!scoring) {
+    subEstado = "1 Pendiente Enlace";
+  } else {
+    const banco =
+      await flujoRegistroService.obtenerBancoPorFlujo(
+        registro.Id
+      );
+
+    const estadoScoring = scoring.Estado?.toLowerCase();
+
+    //Pendiente Tendero
+    if (
+      estadoScoring !== "confirmado" &&
+      (!banco || banco.Pagare_Digital_Firmado !== "si")
+    ) {
+      subEstado = "2 Pendiente Tendero";
+    } else if (estadoScoring === "confirmado" &&(!banco || banco.Pagare_Digital_Firmado !== "si")){
+      subEstado = "2 Pendiente Tendero";
+    }
+    else {
+      // scoring confirmado, pero aún no hay usuario final
+      subEstado = "1 Pendiente Enlace";
+    }
+  }
+}
+
+  const response = {
+    nbCliente: registro.nbCliente,
+    nombreTienda: registro.Nombre_Tienda,
+    Nombres: registro.Nombres,
+    Primer_Apellido: registro.Primer_Apellido,
+    Segundo_Apellido_opcional: registro["2do_Apellido_opcional"],
+    Estado: estado
+  };
+
+  if (estado === 2) response.SubEstado = subEstado;
+  if (estado === 3) response.Cupo = cupo;
+
+  return response;
+}
